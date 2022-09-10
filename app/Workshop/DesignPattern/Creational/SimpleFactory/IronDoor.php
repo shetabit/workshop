c5b48ac5efffb0f8072bdfa6b1f3816e2c9a6f2c
hashem <hashemm364@gmail.com>
2022-09-10 10:06:54 +0430

    Add laravel swagger

---
+++ app/Workshop/DesignPattern/Creational/SimpleFactory/IronDoor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Workshop\DesignPattern\Creational\SimpleFactory;
+
+class IronDoor implements Door
+{
+    protected float $width;
+
+    protected float $height;
+
+    public function __construct(float $width, float $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function getWidth(): float
+    {
+        return $this->width;
+    }
+
+    public function getHeight(): float
+    {
+        return $this->height;
+    }
+}
