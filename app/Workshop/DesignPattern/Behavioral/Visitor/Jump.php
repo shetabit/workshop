0db1796d17120548eac9e95a3048e3997e961095
hashem <hashemm364@gmail.com>
2022-12-04 20:29:07 +0330

    Add visitor pattern

---
+++ app/Workshop/DesignPattern/Behavioral/Visitor/Jump.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Workshop\DesignPattern\Behavioral\Visitor;
+
+class Jump implements AnimalOperation
+{
+    public function visitMonkey(Monkey $monkey)
+    {
+        echo 'Jumped 20 feet high! on to the tree!';
+    }
+
+    public function visitLion(Lion $lion)
+    {
+        echo 'Jumped 7 feet! Back on the ground!';
+    }
+
+    public function visitDolphin(Dolphin $dolphin)
+    {
+        echo 'Walked on water a little and disappeared';
+    }
+}
--- app/Workshop/DesignPattern/Behavioral/Visitor/index.php
+++ app/Workshop/DesignPattern/Behavioral/Visitor/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Workshop\DesignPattern\Behavioral\Visitor\Dolphin;
+use App\Workshop\DesignPattern\Behavioral\Visitor\Jump;
 use App\Workshop\DesignPattern\Behavioral\Visitor\Lion;
 use App\Workshop\DesignPattern\Behavioral\Visitor\Monkey;
 use App\Workshop\DesignPattern\Behavioral\Visitor\Speak;
@@ -14,3 +15,21 @@ $speak = new Speak();
 $monkey->accept($speak);    // Ooh oo aa aa!
 $lion->accept($speak);      // Roaaar!
 $dolphin->accept($speak);   // Tuut tutt tuutt!
+
+
+//***************************************************************
+/**
+ * We could have done this simply by having an inheritance hierarchy for the animals but then we would have to modify
+ * the animals whenever we would have to add new actions to animals. But now we will not have to change them. For example,
+ * let's say we are asked to add the jump behavior to the animals, we can simply add that by creating a new visitor i.e.
+ */
+$jump = new Jump();
+
+$monkey->accept($speak);   // Ooh oo aa aa!
+$monkey->accept($jump);    // Jumped 20 feet high! on to the tree!
+
+$lion->accept($speak);     // Roaaar!
+$lion->accept($jump);      // Jumped 7 feet! Back on the ground!
+
+$dolphin->accept($speak);  // Tuut tutt tuutt!
+$dolphin->accept($jump);   // Walked on water a little and disappeared
